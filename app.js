930aebcfc32810d8bde0b11957618b4127beadef
Ryan Hickey <rhickey94@gmail.com>
2020-04-07 18:24:58 -0400

    this is an abuse of git, i'm sorry

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let size = 0;
+let size = 16;
 let opacity = 0;
 const container = document.querySelector(".container");
 const resetButton = document.querySelector("#reset");
@@ -7,8 +7,7 @@ const getRandomInt = max => {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-const generateGrid = () => {
-    size = parseInt(prompt("How many squares per side?"));
+const generateGrid = (size) => {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     for(let i = 0; i < size; i++) {
         for(let j = 0; j < size; j++) {
@@ -25,13 +24,13 @@ const addListeners = () => {
     squares.forEach(square => {
         let opacity = 0;
         square.addEventListener("mouseover", e => {
-            let red   = getRandomInt(255);
-            let green = getRandomInt(255);
-            let blue  = getRandomInt(255);
+            // let red   = getRandomInt(255);
+            // let green = getRandomInt(255);
+            // let blue  = getRandomInt(255);
             // e.target.classList.toggle("shaded");
-            // let red     = 0;
-            // let green   = 0;
-            // let blue    = 0;
+            let red     = 0;
+            let green   = 0;
+            let blue    = 0;
             e.target.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${opacity < 1 ? opacity += 0.1 : opacity = 0})`;
         });
     });
@@ -41,10 +40,11 @@ const reset = () => {
     while(container.firstChild) {
         container.removeChild(container.lastChild);
     }
-    generateGrid();
+    size = parseInt(prompt("How many squares per side?"));
+    generateGrid(size);
     addListeners();
 }
 resetButton.addEventListener("click", reset);
 
-generateGrid();
+generateGrid(size);
 addListeners();
