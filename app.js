78c5a0f2c539207a69d4d834248c4fe2842f7c41
Ryan Hickey <rhickey94@gmail.com>
2020-04-07 18:05:33 -0400

    added drawing

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
-const size = 50;
+let size = 0;
+let opacity = 0;
 const container = document.querySelector(".container");
-container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+const resetButton = document.querySelector("#reset");
 
 
-for(let i = 0; i < size; i++) {
-    for(let j = 0; j < size; j++) {
-    let square = document.createElement("div");
-    square.setAttribute("class", "square");
-    container.appendChild(square);
-    }   
+const generateGrid = () => {
+    size = parseInt(prompt("How many squares per side?"));
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    for(let i = 0; i < size; i++) {
+        for(let j = 0; j < size; j++) {
+        let square = document.createElement("div");
+        square.setAttribute("class", "square");
+        container.appendChild(square);
+        }   
+    }
 }
+
+const addListeners = () => {
+    const squares = document.querySelectorAll(".square");
+    
+    squares.forEach(square => {
+        square.addEventListener("mouseover", e => {
+            // opacity = (opacity + 10) % 255;
+            // e.target.style.backgroundColor = `rgba(${opacity},${opacity},${opacity},${opacity})`;
+            e.target.classList.toggle("shaded");
+        });
+    });
+}
+
+const reset = () => {
+    while(container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    generateGrid();
+    addListeners();
+}
+resetButton.addEventListener("click", reset);
+
+generateGrid();
+addListeners();
