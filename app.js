d09a868244244571c81d892081a02253fc88e5b0
Ryan Hickey <rhickey94@gmail.com>
2020-04-07 23:46:08 -0400

    refactor to add rainbow mode toggle

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 let size = 16;
-let opacity = 0;
+// let opacity = 0;
 const container = document.querySelector(".container");
 const resetButton = document.querySelector("#reset");
+const rainbowMode = document.querySelector("#rainbow-select");
 
 const getRandomInt = max => {
     return Math.floor(Math.random() * Math.floor(max));
+}
+
+const getOpacity = e => {
+    return parseFloat(e.target.style.backgroundColor.split(" ").pop().slice(-4, -1));
+}
+
+const setColor = e => {
+    let red     = 0;
+    let green   = 0;
+    let blue    = 0;
+    let opacity = getOpacity(e);
+    e.target.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${opacity < 1 ? opacity += 0.1 : opacity = 0.1})`;
+}
+
+const setRainbow = e => {
+    let red   = getRandomInt(255);
+    let green = getRandomInt(255);
+    let blue  = getRandomInt(255);
+    e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
 
 const generateGrid = (size) => {
@@ -18,22 +38,30 @@ const generateGrid = (size) => {
     }
 }
 
-const addListeners = () => {
+const addListeners = (rainbowMode) => {
     const squares = document.querySelectorAll(".square");
-    
-    squares.forEach(square => {
-        let opacity = 0;
-        square.addEventListener("mouseover", e => {
-            // let red   = getRandomInt(255);
-            // let green = getRandomInt(255);
-            // let blue  = getRandomInt(255);
-            // e.target.classList.toggle("shaded");
-            let red     = 0;
-            let green   = 0;
-            let blue    = 0;
-            e.target.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${opacity < 1 ? opacity += 0.1 : opacity = 0})`;
+    if(rainbowMode === "off") {
+        squares.forEach(square => {
+            square.addEventListener("mouseover", setColor);
         });
-    });
+    } else {
+        squares.forEach(square => {
+            square.addEventListener("mouseover", setRainbow);
+        });
+    }
+}
+
+const removeListeners = (rainbowMode) => {
+    const squares = document.querySelectorAll(".square");
+    if(rainbowMode === "off") {
+        squares.forEach(square => {
+            square.removeEventListener("mouseover", setRainbow);
+        });
+    } else {
+        squares.forEach(square => {
+            square.addEventListener("mouseover", setColor);
+        });
+    }
 }
 
 const reset = () => {
@@ -41,10 +69,16 @@ const reset = () => {
         container.removeChild(container.lastChild);
     }
     size = parseInt(prompt("How many squares per side?"));
+    rainbowMode.value = "off";
     generateGrid(size);
-    addListeners();
+    addListeners(rainbowMode.value);
 }
+
 resetButton.addEventListener("click", reset);
+rainbowMode.addEventListener("change", e => {
+    removeListeners(rainbowMode.value);
+    addListeners(rainbowMode.value);
+})
 
 generateGrid(size);
-addListeners();
+addListeners(rainbowMode.value);
